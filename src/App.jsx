815c7961a61698e3eaa32814f939fb38a99828b7
The Angry Coder <79235452+TheAngryCoder2@users.noreply.github.com>
2025-01-04 12:49:42 +0000

    wip

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center bg-white p-6 md:p-12 font-sans">
+      {/* header */}
+      <header className="text-center">
+        <h1 className="text-3xl md:text-4xl font-bold mb-4">
+        Porsche Chronograph 1
+        </h1>
+      </header>
+    </div>
+  )
+}
